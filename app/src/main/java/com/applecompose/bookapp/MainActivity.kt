83ce0f283cd7b36776de53a70c1226135d6cdd2b
Jerry applegarth <applegarth1630@gmail.com>
2022-06-13 20:18:24 -0700

    1.0 setup

---
+++ app/src/main/java/com/applecompose/bookapp/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.applecompose.bookapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.applecompose.bookapp.ui.theme.BookAppTheme
+import com.applecompose.bookapp.ui.theme.newBackgroundColor
+
+class MainActivity : ComponentActivity() {
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContent {
+			BookAppTheme {
+				// A surface container using the 'background' color from the theme
+				Surface(
+					modifier = Modifier.fillMaxSize(),
+					color = MaterialTheme.colors.newBackgroundColor
+				) {
+
+				}
+			}
+		}
+	}
+}
+
+
+
+// https://github.com/JerryApplegarth/Book-App.git
+
